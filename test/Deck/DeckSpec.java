4d65683b07a58e637dcfbd5852d3e0d0f0ee1f85
Alex Rinker <abrinker@colby.edu>
2016-07-26 18:53:47 -0400

    wip deck test

---
+++ test/Deck/DeckSpec.java
@@ -0,0 +1,28 @@
+/* Alex Rinker
+ * DeckSpec.java
+ */
+
+package holdemSpec;
+
+import java.util.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import holdem.*;
+
+public class HandSpec {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RED = "\u001B[31m";
+
+    @Test
+    public void test_build(ArrayList<String> failures) {
+        Deck deck = new Deck();
+        try {
+
+                }
+    }
+
+    public static void main(String args[]) {
+        return;
+    }
+}
